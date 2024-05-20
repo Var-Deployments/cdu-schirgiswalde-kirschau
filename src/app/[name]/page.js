825373ae6566b4eb791a46b5224ca773439039b5
Elia <elia@eliahilse.com>
2024-05-20 23:11:39 +0200

    fix: invididual page layout

--- src/app/[name]/page.js
+++ src/app/[name]/page.js
@@ -49,11 +49,10 @@ export function getStaticData(name) {
 export default function Home({ params }) {
     const {name} = params;
     const data = getStaticData(name);
-    console.log(data);
     return (
         <main
             className="flex min-h-screen max-w-[100vw] flex-col items-center justify-between p-2 pl-6 pt-6 md:p-24 !pb-0 bg-white dark:bg-neutral-950 text-black dark:text-white">
-            <div>
+            <div className="max-w-[100%] md:max-w-[50%]">
             <h1 className="text-5xl font-bold">{data.firstName} {data.lastName}</h1>
             <h5 className="text-2xl font-semibold text-green-600 mt-2">{data.title}</h5>
             <img className="my-4 max-h-[20vh] md:max-h-[35vh] rounded-lg" src={data.img} alt={`Portrait von ${data.firstName} ${data.lastName}`}></img>
@@ -77,8 +76,10 @@ export default function Home({ params }) {
                         </div>
                     ) : null
                 }
-            <BackToHome customURL={`/#${generatePathSegment(data.firstName.toLowerCase())}-${generatePathSegment(data.lastName.toLowerCase())}`} className="transform translate-y-8"></BackToHome>
-            </div>
+                <div className="flex justify-center mt-7 md:mt-5">
+                <BackToHome linkClassName={"bg-opacity-5 hover:bg-opacity-10 transition duration-300 px-4 py-2 bg-black dark:bg-white rounded-lg"} customText={"Zurück"} customURL={`/#${generatePathSegment(data.firstName.toLowerCase())}-${generatePathSegment(data.lastName.toLowerCase())}`} className="inline-block"></BackToHome>
+                </div>
+                </div>
             <Footer instance={namingConfig.instanceName} socialLinks={socialConfig}/>
         </main>
     );
--- src/components/back-to-home.jsx
+++ src/components/back-to-home.jsx
@@ -1,13 +1,13 @@
-const BackToHome = ({ className, customURL="" }) => {
+const BackToHome = ({ className, customURL= "", customText="Zurück zur Homepage", linkClassName= "" }) => {
     return (
         <div className={className + " text-neutral-700 dark:text-neutral-300 text-sm"}>
-            <a href={customURL === "" ? "/" : customURL} className="flex items-center gap-x-2">
+            <a href={customURL === "" ? "/" : customURL} className={`flex items-center gap-x-2 ${linkClassName}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
                      stroke="currentColor" className="w-5 h-5">
                     <path strokeLinecap="round" strokeLinejoin="round"
                           d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"/>
                 </svg>
-                <span>Zurück zur Homepage</span></a>
+                <span>{customText}</span></a>
         </div>
     );
 };
