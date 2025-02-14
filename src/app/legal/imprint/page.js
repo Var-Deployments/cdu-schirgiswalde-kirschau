86447a28d09faf489a2809211bf62f7e8b2d5553
Elia <elia@eliahilse.com>
2025-02-14 15:04:17 +0100

    fix: imprint

--- src/app/legal/imprint/page.js
+++ src/app/legal/imprint/page.js
@@ -26,34 +26,36 @@ export default function Imprint() {
                 <div className="flex flex-col">
                 <h1 className="font-bold text-3xl">Impressum</h1>
                 <span className="font-semibold">Angaben gem. §5 TMG</span>
-                <div className="mt-4 flex flex-col font-light">
-                    <span>{imprintData.name}</span>
-                    <span>{namingConfig.instanceName}</span>
-                    <span>{imprintData.streetAddress}</span>
-                    <div className="flex flex-row gap-x-1">
-                        <span>{imprintData.postalCode},</span>
-                        <span>{imprintData.city}</span>
-                    </div>
-                    <span className="font-bold mt-4">Kontakt</span>
-
-                    <div className="flex flex-row gap-x-1">
-                        <span className="">E-Mail: {imprintData.contact}</span>
-                    </div>
-
-                    <div className="flex flex-col mt-4">
-                        <span className="font-bold">Redaktionell verantwortlich</span>
-                        <span>{imprintData.contentRepresentative.name}</span>
-                        <span>{imprintData.contentRepresentative.streetAddress}</span>
+                    <div className="mt-4 flex flex-col font-light">
+                        <span>{imprintData.name}</span>
+                        <span>{namingConfig.instanceName}</span>
+                        <span>{imprintData.streetAddress}</span>
+                        <span>{imprintData.streetAddressLine2 ?? ""}</span>
                         <div className="flex flex-row gap-x-1">
-                            <span>{imprintData.contentRepresentative.postalCode},</span>
-                            <span>{imprintData.contentRepresentative.city}</span>
+                            <span>{imprintData.postalCode},</span>
+                            <span>{imprintData.city}</span>
+                        </div>
+                        <span className="font-bold mt-4">Kontakt</span>
+
+                        <div className="flex flex-row gap-x-1">
+                            <span className="">E-Mail: {imprintData.contact}</span>
+                        </div>
+
+                        <div className="flex flex-col mt-4">
+                            <span className="font-bold">Redaktionell verantwortlich</span>
+                            <span>{imprintData.contentRepresentative.name}</span>
+                            <span>{imprintData.contentRepresentative.streetAddress}</span>
+                            <span>{imprintData.contentRepresentative.streetAddressLine2 ?? ""}</span>
+                            <div className="flex flex-row gap-x-1">
+                                <span>{imprintData.contentRepresentative.postalCode},</span>
+                                <span>{imprintData.contentRepresentative.city}</span>
+                            </div>
+                        </div>
+                        <BackToHome className="transform translate-y-8"></BackToHome>
+
+                        <div className="flex flex-row gap-x-2">
                         </div>
                     </div>
-                    <BackToHome className="transform translate-y-8"></BackToHome>
-
-                    <div className="flex flex-row gap-x-2">
-                </div>
-                </div>
                 </div>
             </div>
             <Footer instance={namingConfig.officeName} socialLinks={socialConfig}/>
